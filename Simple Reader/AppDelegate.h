100d8397b6f55e1f50a34a2f0c5d72310ed0fa6f
Jonas Höchst <hoechst@jonashoechst.de>
2015-07-08 22:34:25 +0200

    Initial Commit

---
+++ Simple Reader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Simple Reader
+//
+//  Created by Jonas Höchst on 08.07.15.
+//  Copyright (c) 2015 vcp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Simple Reader/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Simple Reader
+//
+//  Created by Jonas Höchst on 08.07.15.
+//  Copyright (c) 2015 vcp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
