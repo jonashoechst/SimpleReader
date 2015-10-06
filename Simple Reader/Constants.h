2028185adb1f25d6e2a6614adcf912eedc34cad3
Jonas Höchst <git@jonashoechst.de>
2015-10-07 00:55:55 +0200

    Introduces Constants.h for global fix app configs; PrefixHeader now adds Constants.h variables, UIKit and Foundation to every class

---
+++ Simple Reader/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  Simple Reader
+//
+//  Created by Jonas Höchst on 06.10.15.
+//  Copyright © 2015 vcp. All rights reserved.
+//
+
+#ifndef Constants_h
+#define Constants_h
+
+
+#endif /* Constants_h */
